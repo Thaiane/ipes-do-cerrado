006e4a0b6587dbb83047cd78a5d2ceecfb857335
Thaiane Braga <thbraga@thoughtworks.com>
2021-10-04 18:33:10 -0300

    Adicionando mais sobre forEach

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const imgs = document.querySelectorAll('img');
 
-imgs.forEach(function(img) {
-  console.log(img);
+imgs.forEach(function(img, index, array) {
+  console.log(img, index, array);
 });
+
+const paragrafos = document.querySelectorAll('p');
+
+paragrafos.forEach(paragrafo => console.log(paragrafo));
